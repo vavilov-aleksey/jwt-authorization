8c45e417d0ff5185c51fab6a6cd0b0190c613c84
Vavilov <aleksey_vavilov@mail.ru>
2021-06-15 23:09:57 +0400

    add base settings for server

---
+++ server/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookiesParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
+const PORT = process.env.SERVER_PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(cookiesParser());
+app.use(cors());
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.URL_DB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+start();
